0f3d5109277b074494bd7ede894289cc33b22229
KarimTarabishy <tarabishy_2020@hotmail.com>
2016-04-29 18:51:30 +0200

    Fix openshift app start

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,11 +1,12 @@
 var winston = require('winston');
 winston.emitErrs = true;
 
+var log_location = process.env.OPENSHIFT_LOG_DIR || "./logs/";
 var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'debug',
-            filename: './logs/all-logs.log',
+            filename: log_location + 'all-logs.log',
             json: true,
             maxsize: 5242880, //5MB
             maxFiles: 5,
